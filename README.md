209d98fb5ef034e0b5e2bd5cbefd4d3087424f73
garcialeonelizabeth13-hash <garcialeonelizabeth13@gmail.com>
2026-07-18 03:53:56 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Reciclaje
+
+Aplicación Vue 3 para gestión de reciclaje.
+
+## Configuración del Proyecto
+
+```sh
+npm install
+```
+
+### Compilar y recargar en caliente para desarrollo
+
+```sh
+npm run dev
+```
+
+### Compilar y minificar para producción
+
+```sh
+npm run build
+```
+
+### Previsualizar la compilación de producción
+
+```sh
+npm run preview
+```
+
+## Tecnologías
+
+- Vue 3
+- Vite
---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>♻️ App de Reciclaje</h1>
+    </header>
+    <main>
+      <p>Bienvenido a tu aplicación de reciclaje</p>
+      <p>Este proyecto está listo para que comiences a desarrollar.</p>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+header {
+  margin-bottom: 2rem;
+}
+
+h1 {
+  color: #42b983;
+}
+
+main {
+  padding: 0 2rem;
+}
+
+p {
+  font-size: 1.1rem;
+  line-height: 1.6;
+  margin: 1rem 0;
+}
+</style>
